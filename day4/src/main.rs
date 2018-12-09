e48b0e080f338b640ec4429a747bef708a84462f
Fabien JUIF <fabien.juif@gmail.com>
2018-12-09 16:30:59 +0100

    :construction: day4 / part2 / refactor

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -18,7 +18,14 @@ struct Record {
     description: String,
 }
 
-fn get_records() -> Vec<Record> {
+struct Guard {
+    id: String,
+    minutes: Vec<(i64, i64)>,
+    most_minute_asleep: (i64, i64),
+    total_asleep: i64,
+}
+
+fn get_guards() {
     let content = fs::read_to_string(FILE_NAME).unwrap();
     let re = Regex::new(r"\[(\d+-\d+-\d+) (\d+):(\d+)\]( Guard #)?(\d+)? (.*)").unwrap();
     let mut records: Vec<Record> = Vec::new();
@@ -52,105 +59,58 @@ fn get_records() -> Vec<Record> {
     }
 
     records.sort_by(|record_a, record_b| record_a.date_time.cmp(&record_b.date_time));
+    println!("records size: {}", records.len());
 
-    records
-}
-
-fn part1_get_most_sleep(records: &[Record]) -> String {
-    let mut sleeps: HashMap<String, i64> = HashMap::new();
-    let mut last_guard_id = String::from("");
-    let mut last_date_time = Utc::now();
-
-    for record in records {
-        let Record { guard_id, date_time, description, .. } = record;
-
-        if !guard_id.is_empty() { // empty guard_id means we are changing guard
-            last_guard_id = guard_id.clone();
-        }
-
-        if description == "falls asleep" {
-            last_date_time = date_time.clone();
-        }
-
-        if description == "wakes up" {
-             let sleep = sleeps.entry(last_guard_id.clone()).or_insert(0);
-            *sleep += (*date_time - last_date_time).num_minutes() + 1;
-        }
-    }
-
-    let mut sorted_sleeps = Vec::new();
-    for (guard_id, sleep) in sleeps {
-        sorted_sleeps.push((guard_id, sleep));
-    }
-    sorted_sleeps.sort_by(|sleep_a, sleep_b| sleep_b.1.cmp(&sleep_a.1));
-
-    let (guard_id, _) = sorted_sleeps.get(0).unwrap();
-
-    guard_id.to_string()
-}
-
-fn part1_get_most_seen_minute(records: &[Record], most_sleep_guard_id: String) -> i64 {
-    println!("must sleep: #{}", most_sleep_guard_id);
-
-    let mut last_guard_id = String::from("");
+    let mut guards: HashMap<String, Guard> = HashMap::new();
+    let mut current_guard: &mut Guard = &mut Guard { id: String::from(""), minutes: Vec::new(), most_minute_asleep: (0, 0), total_asleep: 0 };
     let mut last_date_time = Utc::now();
     let mut last_minute = String::from("");
-    let mut last_fulldate = String::from("");
-    let mut minutes = HashMap::new();
+    let mut minutes: HashMap<i64, i64> = HashMap::new();
     let mut sorted_minutes = Vec::new();
 
     for record in records {
-        let Record { guard_id, fulldate, date_time, description, minute, .. } = record;
+        let Record { guard_id, date_time, description, minute, .. } = record;
 
         if !guard_id.is_empty() { // empty guard_id means we are changing guard
-            last_guard_id = guard_id.clone();
-        }
+            current_guard = guards.entry(guard_id.clone()).or_insert(Guard {
+                id: guard_id.clone(),
+                minutes: Vec::new(),
+                most_minute_asleep: (0, 0),
+                total_asleep: 0,
+            });
 
-        if last_guard_id != most_sleep_guard_id {
-            continue;
+            for (minute, times) in minutes.iter() {
+                sorted_minutes.push((minute.clone(), times.clone()));
+            }
+            sorted_minutes.sort_by(|minute_a, minute_b| minute_b.1.cmp(&minute_a.1));
+
+            current_guard.minutes = sorted_minutes.drain(..).collect();
+            current_guard.most_minute_asleep = match current_guard.minutes.get(0) {
+                Some(value) => value.clone(),
+                None => (0, 0)
+            };
+
+            minutes.clear();
+            sorted_minutes.clear();
         }
 
         if description == "falls asleep" {
             last_date_time = date_time.clone();
             last_minute = minute.clone();
-            last_fulldate = fulldate.clone();
         }
 
         if description == "wakes up" {
-            let num_minutes = (*date_time - last_date_time).num_minutes();
+            current_guard.total_asleep += (date_time - last_date_time).num_minutes() + 1;
+
+            let num_minutes = (date_time - last_date_time).num_minutes();
 
             for min in 0..num_minutes {
                 *minutes.entry(last_minute.parse::<i64>().unwrap() + min).or_insert(0) += 1;
             }
         }
     }
-
-    for (minute, times) in minutes {
-        sorted_minutes.push((minute, times));
-    }
-    sorted_minutes.sort_by(|minute_a, minute_b| minute_b.1.cmp(&minute_a.1));
-
-    sorted_minutes.get(0).unwrap().0
-}
-
-fn part1(records: &[Record]) {
-    let most_sleep_guard_id = part1_get_most_sleep(records);
-    let most_seen_minute = part1_get_most_seen_minute(records, most_sleep_guard_id.clone());
-
-    println!(
-        "#{} - most seen asleep: {} --> {}x{} => {}",
-        most_sleep_guard_id,
-        most_seen_minute,
-        most_sleep_guard_id,
-        most_seen_minute,
-        most_sleep_guard_id.parse::<i64>().unwrap() * most_seen_minute,
-    )
 }
 
 fn main() {
-    let records = get_records();
-    println!("records size: {}", records.len());
-
-    println!("-- part1 --");
-    part1(records.as_slice());
+    get_guards();
 }
