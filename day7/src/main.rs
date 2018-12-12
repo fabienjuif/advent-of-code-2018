121a0029573741d99de085a63f4c5e17b08d7c05
Fabien JUIF <fabien.juif@gmail.com>
2018-12-12 23:30:49 +0100

    :tada: day7 / step2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -5,7 +5,6 @@ use std::error::Error;
 use std::result;
 use regex::Regex;
 use std::collections::HashMap;
-use std::option::Option;
 
 const FILE_NAME: &str = "./input.txt";
 
@@ -14,11 +13,19 @@ type Result<T> = result::Result<T, Box<Error>>;
 #[derive(Clone, Debug)]
 struct Step {
     name: String,
+    process_time: i32,
     next: Vec<String>,
     previous: Vec<String>,
 }
 
-fn get_next(steps: &HashMap<String, Step>, visited: &Vec<String>) -> Option<Step> {
+#[derive(Clone, Debug)]
+struct Worker {
+    id: i32,
+    step_name: Option<String>,
+    available_at: i32,
+}
+
+fn part1_get_next(steps: &HashMap<String, Step>, visited: &mut Vec<String>) -> Option<Step> {
     let mut filtered_steps: Vec<&Step> = steps
         .iter()
         .map(|(_, step)| step)
@@ -28,12 +35,32 @@ fn get_next(steps: &HashMap<String, Step>, visited: &Vec<String>) -> Option<Step
     filtered_steps.sort_by_key(|step| &step.name);
 
     match filtered_steps.get(0) {
-        Some(step) => Option::Some((*step).clone()),
-        None => Option::None
+        Some(step) => {
+            visited.push((*step).name.clone());
+            Some((*step).clone())
+        }
+        None => None
     }
 }
 
-fn main() -> Result<()> {
+fn part1(steps: &HashMap<String, Step>) {
+    let mut steps = steps.clone();
+
+    let mut visited = Vec::<String>::new();
+    while let Some(next) = part1_get_next(&steps, &mut visited) {
+        print!("{}", next.name);
+
+        for (name, step) in steps.clone().iter() {
+            if step.previous.contains(&next.name) {
+                steps.get_mut(name).unwrap().previous.retain(|name| name != &next.name);
+            }
+        }
+    }
+
+    println!("");
+}
+
+fn create_steps() -> Result<HashMap<String, Step>> {
     let content = fs::read_to_string(FILE_NAME)?;
     let re = Regex::new(r"Step (.) must be finished before step (.) can begin.*")?;
 
@@ -51,6 +78,7 @@ fn main() -> Result<()> {
             .or_insert(Step {
                 name: String::from(before_name),
                 next: vec![String::from(after_name)],
+                process_time: before_name.as_bytes()[0] as i32 - 64,
                 previous: vec![],
             });
 
@@ -59,23 +87,64 @@ fn main() -> Result<()> {
             .or_insert(Step {
                 name: String::from(after_name),
                 next: vec![],
+                process_time: after_name.as_bytes()[0] as i32 - 64,
                 previous: vec![String::from(before_name)],
             });
     }
 
-    let mut visited = Vec::<String>::new();
-    while let Some(next) = get_next(&steps, &visited) {
-        visited.push(next.name.clone());
-        print!("{}", next.name);
+    Ok(steps)
+}
 
-        for (name, step) in steps.clone().iter() {
-            if step.previous.contains(&next.name) {
-                steps.get_mut(name).unwrap().previous.retain(|name| name != &next.name);
+fn part2(steps: &HashMap<String, Step>, workers_count: i32) {
+    let mut steps = steps.clone();
+    let mut workers = vec![];
+
+    for id in 0..workers_count {
+        workers.push(
+            Worker {
+                id: id as i32,
+                step_name: None,
+                available_at: 0,
+            },
+        );
+    }
+
+    let mut visited = vec![];
+    let mut seconds = -1;
+
+    while visited.len() < steps.len() {
+        seconds += 1;
+
+        for worker in workers.iter_mut() {
+            if worker.available_at > seconds { continue; }
+
+            if let Some(step_name) = worker.step_name.clone() {
+                for (name, step) in steps.clone().iter() {
+                    if step.previous.contains(&step_name) {
+                        steps.get_mut(name).unwrap().previous.retain(|name| name != &step_name);
+                    }
+                }
+            }
+
+            if let Some(next) = part1_get_next(&steps, &mut visited) {
+                worker.step_name = Some(next.name.clone());
+                worker.available_at = seconds + 60 + next.process_time;
             }
         }
     }
 
-    println!("");
+    workers.sort_by_key(|worker| worker.available_at);
+    println!("{}", workers.pop().unwrap().available_at);
+}
+
+fn main() -> Result<()> {
+    let steps = create_steps()?;
+
+    println!("--part 1--");
+    part1(&steps);
+
+    println!("--part 2--");
+    part2(&steps, 5);
 
     Ok(())
 }
